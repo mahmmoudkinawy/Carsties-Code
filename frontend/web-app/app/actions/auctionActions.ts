cfa0334865124c53b12749f0601a8d7ef1e2bcd5
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2024-03-04 00:32:05 +0200

    End of section 9

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -1,6 +1,7 @@
 'use server';
 
 import { PagedResult, Auction } from '@/types';
+import { getTokenWorkaround } from './authActions';
 
 export async function getData(query: string): Promise<PagedResult<Auction>> {
   const res = await fetch(`http://localhost:6001/search${query}`);
@@ -10,4 +11,27 @@ export async function getData(query: string): Promise<PagedResult<Auction>> {
   }
 
   return res.json();
+}
+
+export async function UpdateAuctionTest() {
+  const data = {
+    mileage: Math.floor(Math.random() * 100000) + 1,
+  };
+
+  const token = await getTokenWorkaround();
+
+  const res = await fetch(`http://localhost:6001/auctions`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token?.access_token}`,
+    },
+    body: JSON.stringify(data),
+  });
+
+  if (!res.ok) {
+    return { status: res.status, message: res.statusText };
+  }
+
+  return res.statusText;
 }
